6f5303b695b5565561d396cdcc75ecb472136ff8
jebasingh-joshua <jeb@friendlycares.com>
2026-01-24 14:55:42 +0530

    after login keypad opens issue fixed

--- lib/widgets/auth_dialog.dart
+++ lib/widgets/auth_dialog.dart
@@ -152,6 +152,8 @@ class _AuthDialogState extends State<AuthDialog> {
       }
 
       if (!mounted) return;
+      FocusManager.instance.primaryFocus?.unfocus();
+      await SystemChannels.textInput.invokeMethod('TextInput.hide');
       Navigator.of(context).pop();
     } on AuthApiException catch (e) {
       setState(() => _error = e.message);
